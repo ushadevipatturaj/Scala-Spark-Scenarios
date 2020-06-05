87caecbb284a7ecd2c67bfc0e51a6b8df58cdca6
ushapatturaj <ushadevipatturaj@gmail.com>
2020-06-05 19:55:52 +0200

    Scala Spark Scenarios - identify overlap time in scala

--- src/main/scala/com/SparkScalaScenarios/Part1/SparkIdentifyOverlaps.scala
+++ src/main/scala/com/SparkScalaScenarios/Part1/SparkIdentifyOverlaps.scala
@@ -16,41 +16,16 @@ object SparkIdentifyOverlaps extends App with  Context {
     customer(110,"Channel1","18:45:12","18:50:32"))
   import spark.implicits._
   val customerDataFrame = customerSeq.toDF()
-  val win1 = Window.partitionBy($"Customer_Id").orderBy($"timeStart", $"timeEnd")
-  val win2 = Window.partitionBy($"Customer_Id", $"group_id")
-  val overlappedDF = customerDataFrame.
-      withColumn("group_id", when(
-        $"timeStart".between(lag($"timeStart", 1).over(win1), lag($"timeEnd", 1).over(win1)), null
-      ).otherwise(monotonically_increasing_id)
-      ).
-    withColumn("group_id", last($"group_id", ignoreNulls=true).
-        over(win1.rowsBetween(Window.unboundedPreceding, 0))
-    )
-    .withColumn("timeStart1", min($"timeStart").over(win2)).
-     withColumn("timeEnd1", max($"timeEnd").over(win2)).
-     orderBy("Customer_Id", "timeStart", "timeEnd")
+  val win1 = Window.partitionBy("Customer_Id").orderBy("timeStart","timeEnd")
+  val win2 = Window.partitionBy("Customer_Id","groupid")
 
-  overlappedDF.select("Customer_Id", "Channel","timeStart1", "timeEnd1").distinct().show()
-
-//  df.
-//    withColumn("group_id", when(
-//      $"begin_dt".between(lag($"begin_dt", 1).over(win1), lag($"end_dt", 1).over(win1)), null
-//    ).otherwise(monotonically_increasing_id)
-//    ).
-//    withColumn("group_id", last($"group_id", ignoreNulls=true).
-//      over(win1.rowsBetween(Window.unboundedPreceding, 0))
-//    ).
-//    withColumn("begin_dt2", min($"begin_dt").over(win2)).
-//    withColumn("end_dt2", max($"end_dt").over(win2)).
-//    orderBy("key", "begin_dt", "end_dt").
-//    show
-//  Expected Output:
-//
-//    Customer_Id Channel Start time End time
-//      100 Channel1 19:00:30 19:55:35
-//  100 Channel1 20:00:10 20:30:00
-//  100 Channel2 15:05:10 15:30:30
-//  110 Channel1 18:30:20 18:32:50
-//  110 Channel1 18:40:55 18:59:10
+  val overlappedDF = customerDataFrame.withColumn("groupid",
+    when($"timeStart".between(lag("timeStart",1).over(win1),lag("timeEnd",1).over(win1)),null)
+    .otherwise(monotonically_increasing_id()))
+      .withColumn("groupid",last("groupid",ignoreNulls = true).over(win1.rowsBetween(Window.unboundedPreceding,0)))
+      .withColumn("timeStart1",min("timeStart").over(win2))
+      .withColumn("timeEnd1",max("timeEnd").over(win2))
+      .orderBy("Customer_Id","Channel","timeStart1","timeEnd1")
+  overlappedDF.select("Customer_Id","Channel","timeStart1","timeEnd1").distinct().show()
 
 }
